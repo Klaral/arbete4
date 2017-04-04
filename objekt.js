39f77eafcd13d271b2dc15adc21525b08fa10ad6
Klaral <klaral@users.noreply.github.com>
2017-04-04 15:07:44 +0200

    hh

--- objekt.js
+++ objekt.js
@@ -7,14 +7,14 @@ window.onload = function(){
     /**lägga till**/
    
     addButton.addEventListener("click", function(event){
-     var pris = document.getElementById("inputPris");
+    var pris = document.getElementById("inputPris");
   var fb = firebase.database();
   var btnNamn = document.getElementById("inputNamn");
   var btnPris = document.getElementById("btnPris");
        
         
         
- // var currDate = new Date();
+
   var data = {
             namn: btnNamn.value,
             pris: Number(pris.value)
@@ -39,8 +39,7 @@ window.onload = function(){
     table.innerHTML = "";
     snapshot.forEach(function(child){
       var tr = document.createElement("tr");//child is a object, child.key to get id
-      tr.innerHTML = `<td>${child.val().namn}</td>
-      <td>${child.val().pris}</td>`
+      tr.innerHTML = `<td>${child.val().namn}</td> <td>${child.val().pris}</td>`
       table.appendChild(tr);
       console.log(child.val())
     })
