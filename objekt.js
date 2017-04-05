97739fe8395ba63f4fa997abc653539beabff208
Klaral <klaral@users.noreply.github.com>
2017-04-05 11:03:51 +0200

    df

--- objekt.js
+++ objekt.js
@@ -57,17 +57,17 @@
 
 
  window.addEventListener('load', function() {
-			let namn = document.getElementById('inputNamn');
-			let familj = document.getElementById('inputFamilj');
-			let antal = document.getElementById('inputAntal');
-			let färg = document.getElementById('inputFärg');
-			let addButton = document.getElementById('addButton');
-			let tableVisaDjur = document.getElementById('tableVisaDjur');
-			let btnSortNamn = document.getElementById('btnSortNamn');
-			let btnSortFamilj = document.getElementById('btnSortFamilj');
-			let btnSortAntal = document.getElementById('btnSortAntal');
-			let btnSortFärg = document.getElementById('btnSortFärg');
-			let inputAntalResultat = document.getElementById('inputAntalResultat');
+			var namn = document.getElementById('inputNamn');
+			var familj = document.getElementById('inputFamilj');
+			var antal = document.getElementById('inputAntal');
+			var färg = document.getElementById('inputFärg');
+			var addButton = document.getElementById('addButton');
+			var tableVisaDjur = document.getElementById('tableVisaDjur');
+			var btnSortNamn = document.getElementById('btnSortNamn');
+			var btnSortFamilj = document.getElementById('btnSortFamilj');
+			var btnSortAntal = document.getElementById('btnSortAntal');
+			var btnSortFärg = document.getElementById('btnSortFärg');
+			var inputAntalResultat = document.getElementById('inputAntalResultat');
 			
 			
 			addButton.addEventListener('click', function(event) {
@@ -82,12 +82,12 @@
 			
 			firebase.database().ref('djur/').on('child_added', function(snapshot, prevChildKey) {
 				console.log('Första gången eller ändring i databasen. prevChildKey: ' + prevChildKey);
-				let data = snapshot.val();
+				var data = snapshot.val();
 				//console.log('data:', data);
 				addAnimalToTable(data);
 			});
 			function addAnimalToTable(data) {
-				let tr = document.createElement('tr');
+				var tr = document.createElement('tr');
 				tr.innerHTML = `<td>${data.namn}</td> <td>${data.familj}</td> <td>${data.antal}</td> <td style="width: 50px; background-color: ${data.färg};"></td>`;
 				tableVisaDjur.appendChild(tr);
 			}
@@ -111,9 +111,9 @@
 			
 			inputAntalResultat.addEventListener('keypress', function(event) {
 				if( event.keyCode == 13 ) {
-					let antal = Number(inputAntalResultat.value);
+					var antal = Number(inputAntalResultat.value);
 					tableVisaDjur.innerHTML = '';
-					//console.log('inputAntalResultat: antal=' + antal);
+					console.log('inputAntalResultat: antal=' + antal);
 					if( isNaN(antal) ) {
 						// varna användaren
 					} else {
